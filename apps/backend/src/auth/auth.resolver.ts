6178d3d37f4307836a2361589a22ca7af990fd55
Enes Aydin <enesaydindev@gmail.com>
2023-06-26 02:05:32 +0300

    added signout query

--- apps/backend/src/auth/auth.resolver.ts
+++ apps/backend/src/auth/auth.resolver.ts
@@ -1,8 +1,9 @@
-import { Args, Mutation, Query, Resolver } from "type-graphql"
+import { Args, Ctx, Mutation, Query, Resolver } from "type-graphql"
 import { Service } from "typedi"
 import { SignupDto } from "./dto/signup.dto"
 import { AuthService } from "./auth.service"
 import { SigninDto } from "./dto/signin.dto"
+import { GraphQLContext } from "../types/graphql-context.type"
 
 @Service()
 @Resolver()
@@ -10,14 +11,38 @@ export class AuthResolver {
   constructor(private authService: AuthService) {}
 
   @Query(() => String)
-  async signin(@Args() signinData: SigninDto) {
+  async signin(
+    @Args() signinData: SigninDto,
+    @Ctx() { reply }: GraphQLContext
+  ) {
     const token = await this.authService.signin(signinData)
+    reply.setCookie("userJwt", token, {
+      maxAge: 60 * 60 * 24 * 7,
+      httpOnly: true,
+      sameSite: "lax",
+      secure: true,
+    })
     return token
   }
 
   @Mutation(() => String)
-  async signup(@Args() signupData: SignupDto) {
+  async signup(
+    @Args() signupData: SignupDto,
+    @Ctx() { reply }: GraphQLContext
+  ) {
     const token = await this.authService.signup(signupData)
+    reply.setCookie("userJwt", token, {
+      maxAge: 60 * 60 * 24 * 7,
+      httpOnly: true,
+      sameSite: "lax",
+      secure: true,
+    })
     return token
+  }
+
+  @Query(() => Boolean)
+  signout(@Ctx() { reply }: GraphQLContext) {
+    reply.clearCookie("userJwt")
+    return true
   }
 }
--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -2,11 +2,13 @@ import * as dotenv from "dotenv"
 dotenv.config()
 
 import "reflect-metadata"
-import Fastify from "fastify"
+import Fastify, { FastifyReply, FastifyRequest } from "fastify"
 import { buildSchema } from "type-graphql"
 import Container from "typedi"
 import mercurius from "mercurius"
 import { AuthResolver } from "./auth/auth.resolver"
+import fastifyCors from "@fastify/cors"
+import fastifyCookie from "@fastify/cookie"
 
 async function main() {
   const app = Fastify()
@@ -14,10 +16,21 @@ async function main() {
     resolvers: [AuthResolver],
     container: Container,
   })
+  app.register(fastifyCors, {
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  })
+  app.register(fastifyCookie)
   app.register(mercurius, {
     schema,
     jit: 1,
     ide: true,
+    context: async (req: FastifyRequest, reply: FastifyReply) => {
+      return {
+        req,
+        reply,
+      }
+    },
   })
   const url = await app.listen({ port: 4000 })
   console.log(`Server running at ${url}/graphiql`)
